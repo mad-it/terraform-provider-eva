8f26eb3a778907adbc3f1b06e362c294161f262b
Willian Souza Goncalves <willian.souzagoncalves@grandvision.com>
2022-03-11 16:17:04 +0100

    fix: added more realistic example

--- examples/resources/eva_stencil.tf
+++ examples/resources/eva_stencil.tf
@@ -4,9 +4,25 @@ resource "eva_stencil" "test" {
     language_id     = 1
     country_id      = 1
     header          = "<header></header>"
-    template        = "<html></html>"
+    template        = <<EOT
+<!DOCTYPE html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Hello World</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  </head>
+  <body>
+    <h1>We got something working</h1>
+  </body>
+</html>
+EOT
     footer          = "<footer></footer>"
-    helpers         = "function someJavascriptFunction() {}; someJavascriptFunction();"
+    helpers         = "function someJavascriptFunction() {}"
     type            = 1
     layout          = 1
     destination     = 1
